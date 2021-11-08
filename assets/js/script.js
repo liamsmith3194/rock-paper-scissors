93913ff6af0c5f338758969904ed3712cb70d9f9
liamsmith3194 <smith.liam1994@gmail.com>
2021-11-08 22:04:18 +0000

    plaaceholder icons in but still need to postion based on screen size. JavaScript to generate answer is logging to console

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function roundNumber(){
+
+}
+
+
+function usersAnswer(){
+
+}
+
+
+/**
+ * Generates random answers from rock, paper or scissors
+ */
+
+function computersAnswer(){
+    let random = ["rock", "paper", "scissors"];
+    return random[Math.floor(Math.random() * 3)];
+}
+
+console.log(computersAnswer(3));
+
+
+function compareAnswers(){
+
+}
+
+function roundsWon(){
+
+}
+
+function roundsDrawn(){
+      
+}
+
+function roundsLost(){
+      
+}
+
