b640452a3879e4edfeab9b93d725074b62e5c17c
liamsmith3194 <smith.liam1994@gmail.com>
2021-11-10 19:26:19 +0000

    Code to record button press working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
+       document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            let gesture = this.getAttribute("data-type");
+                alert(`you clicked ${gesture}`);
+        })
+    }
+})
+
+
+
 function roundNumber(){
 
 }
 
 
-function usersAnswer(){
+function userAnswer(){
 
 }
 
@@ -12,27 +25,39 @@ function usersAnswer(){
  * Generates random answers from rock, paper or scissors
  */
 
-function computersAnswer(){
+function computerAnswer(){
     let random = ["rock", "paper", "scissors"];
     return random[Math.floor(Math.random() * 3)];
 }
+console.log(computerAnswer(3));
 
-console.log(computersAnswer(3));
+
+function displayComputerAnswer() {
+    document.getElementsByClassName("computer-answer").textContent = "Hi";
+}
 
 
 function compareAnswers(){
 
 }
 
-function roundsWon(){
+function increaseRoundWon() {
 
+    let roundsWon = parseInt(document.getElementsByClassName("win").innerText);
+    document.getElementsByClassName("win").innerText = ++roundsWon;
+    
 }
 
-function roundsDrawn(){
-      
+function increaseRoundDrawn() {
+
+    let roundsDrawn = parseInt(document.getElementsByClassName("draw").innerText);
+    document.getElementsByClassName("draw").innerText = ++roundsDrawn;
+    
 }
 
-function roundsLost(){
-      
-}
+function increaseRoundLost() {
 
+    let roundslost = parseInt(document.getElementsByClassName("lose").innerText);
+    document.getElementsByClassName("lose").innerText = ++roundslost;
+    
+}
